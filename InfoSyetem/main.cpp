7a655574f11eca1c0a0bfa9a0073cb2a35f63d2f
unknown <wujiahong19981022@outlook.com>
2021-08-07 13:50:32 +0800

    Qlabel Hello例子

--- InfoSyetem/main.cpp
+++ InfoSyetem/main.cpp
@@ -1,11 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include <QLabel>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
+    QLabel *label = new QLabel("<h2><font color='red'>Hello</font>, world!<h2>");
+    label->show();
     return a.exec();
 }
