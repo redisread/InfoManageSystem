bd9c0e27aafa00d0504490a233fd942df95ea930
unknown <wujiahong19981022@outlook.com>
2021-08-14 15:37:48 +0800

    添加MySQL数据库的配置信息

---
+++ InfoSyetem/Config/DatabaseConfig.h
@@ -0,0 +1,18 @@
+#ifndef DATABASECONFIG_H
+#define DATABASECONFIG_H
+
+#include <QString>
+
+
+namespace DatabaseConfig {
+    static const QString HOST_NAME = "496e43891749bbb1.c.cloudtogo.cn";
+    static const int PORT = 35236;
+    static const QString USER_NAME = "root";
+    static const QString PASSWORD = "123456";
+    static const QString DATABASE_NAME ="InfoSystem";
+    static const QString USER_TABLE_NAME = "User";
+}
+
+
+
+#endif
